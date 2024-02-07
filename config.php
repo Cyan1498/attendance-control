b025f215fbdcbfef703e414abc78a092fa59cc41
Cyan <yancerna1498@gmail.com>
2024-02-07 18:31:45 -0500

    Sistema de control de asistencia iniciando

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+date_default_timezone_set('America/Lima');
+// Inicia una sesión de PHP, lo que permite almacenar y recuperar datos de sesión para usuarios.
+session_start();
+// Define: función o palabra reserva se utiliza para definir constantes. Tiene dos argumentos:
+//define('CONSTANTE', 'valor_constante)
+define('RUTA', 'http://localhost/projects/php/asistencia/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_NAME', 'asistencia');
+define('DB_PASS', '');
+?>
