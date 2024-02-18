84608a6b0d5bcbee9c5f17d96332cc22cde06356
Cyan <yancerna1498@gmail.com>
2024-02-18 08:45:05 -0500

    grafico lineas

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     totales()
+    cargarGraficoAsistenciasSemanales();
 })
 function totales() {
     axios.get(ruta + 'controllers/adminController.php?option=totales')
@@ -14,4 +15,59 @@ function totales() {
         .catch(function (error) {
             console.log(error);
         });
+}
+
+function cargarGraficoAsistenciasSemanales() {
+    axios.get(ruta + 'controllers/adminController.php?option=weekAsistencias')
+        .then(function (response) {
+            const data = response.data;
+            console.log(data);
+            // Aquí se deben procesar los datos de asistencias semanales y crear el gráfico
+            crearGraficoAsistenciasSemanales(data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+function crearGraficoAsistenciasSemanales(data) {
+    // Configurar el gráfico con los datos de asistencias semanales
+    var options = {
+        chart: {
+            height: 380,
+            type: 'area',
+            stacked: false,
+        },
+        stroke: {
+            curve: 'straight'
+        },
+        dataLabels: {
+            enabled: true,
+            formatter: function(val) {
+                return val.toFixed(0);
+            },
+            offsetY: -5,
+            style: {
+                fontSize: '12px',
+                colors: ["#304758"]
+            }
+        },
+        series: [{
+            name: 'Asistencias',
+            data: data
+        }],
+        xaxis: {
+            categories: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+        },
+        tooltip: {
+            followCursor: true
+        },
+        fill: {
+            opacity: 1,
+        }
+    };
+
+    // Crear el gráfico
+    var chart = new ApexCharts(document.querySelector('#chart-container'), options);
+    chart.render();
 }
